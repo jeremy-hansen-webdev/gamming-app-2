f18816299a780441dc433d40b0dbc366de18e054
jeremy-hansen-webdev <jeremy.j.hansen3420@gmail.com>
2025-12-10 08:31:28 -0500

    created nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,14 @@ import LightDarkButton from './LightDarkButton';
 
 const NavBar = () => {
   return (
-    <div>
-      <LightDarkButton />
-      <img className="w-20" src="public\Logo\logo.webp" alt="" />
-      <h1 className="bg-amber-200 dark:bg-blue-900">Gaming</h1>
+    <div className="flex justify-between items-center px-5 bg-amber-300 dark:bg-zinc-950">
+      <div className='flex items-center'>
+        <img className="w-20" src="public\Logo\logo.webp" alt="" />
+        <h1 className="text-2xl dark:text-zinc-200">Gaming</h1>
+      </div>
+      <div>
+        <LightDarkButton />
+      </div>
     </div>
   );
 };
