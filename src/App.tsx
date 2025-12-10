4d674c0efa4fabe6d89424fdbaa63ed73d965ec5
jeremy-hansen-webdev <jeremy.j.hansen3420@gmail.com>
2025-12-10 07:53:23 -0500

    setting up images

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import GameList from './components/GameList';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
+      <NavBar />
       <GameList />
     </>
   );
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+
+const NavBar = () => {
+  return (
+    <div>
+      <img className="w-20" src="public\Logo\logo.webp" alt="" />
+      <h1>Gaming</h1>
+    </div>
+  )
+}
+
+export default NavBar
