ac448bf3847094b422ac168757e79f86b6069936
jeremy-hansen-webdev <jeremy.j.hansen3420@gmail.com>
2025-12-26 09:10:29 -0500

    testing vercel deployment

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,9 +3,9 @@ import LightDarkButton from './LightDarkButton';
 const NavBar = () => {
   return (
     <div className="flex justify-between items-center px-5 bg-amber-300 dark:bg-zinc-950">
-      <div className='flex items-center'>
+      <div className="flex items-center">
         <img className="w-20" src="public\Logo\logo.webp" alt="" />
-        <h1 className="text-2xl dark:text-zinc-200">Gaming</h1>
+        <h1 className="text-2xl dark:text-zinc-200">Jeremy's Gaming App</h1>
       </div>
       <div>
         <LightDarkButton />
