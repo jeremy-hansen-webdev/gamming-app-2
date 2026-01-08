6ae6d4584c50b6f78dc4a8b7e1353db8a337e2d2
jeremy-hansen-webdev <jeremy.j.hansen3420@gmail.com>
2026-01-08 13:30:17 -0500

    Added home link to logo on navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import LightDarkButton from './LightDarkButton';
 
 const NavBar = () => {
   return (
     <div className="flex justify-between items-center px-5 bg-amber-300 dark:bg-zinc-950">
       <div className="flex items-center">
-        <img className="w-20" src="public\Logo\logo.webp" alt="" />
+        <Link to={'/'}>
+          <img className="w-20" src="/Logo/logo.webp" alt="" />
+        </Link>
         <h1 className="text-2xl dark:text-zinc-200">Jeremy's Gaming App</h1>
       </div>
       <div>
